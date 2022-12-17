5e14a059409f363fc73b1e7570c0a3ab44434f58
Marcello Vitali <Marcello.Vitali@partner.bmw.de>
2022-12-17 17:52:13 +0100

    Add project files.

---
+++ MQRabbitCmdLet/SendQueueResult.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MQRabbitCmdLet
+{
+    internal class SendQueueResult
+    {
+        public string QueueName { get; set; }
+        public string Message { get; set; }
+        public string MQHost { get; set; } = "localhost";
+
+        public string SendMessage()
+        {
+            var factory = new ConnectionFactory() { HostName = MQHost };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: QueueName,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string _message = Message;
+                var body = Encoding.UTF8.GetBytes(_message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: QueueName,
+                                     basicProperties: null,
+                                     body: body);
+                string result = String.Format(" [x] Sent {0}", _message);
+
+                return result;
+            }
+        }
+    }
+}
---
+++ MQRabbitCmdLet/ToExchange.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using RabbitMQ.Client;
+//TODO: IMPLEMENT
+namespace MQRabbitCmdLet
+{
+    [Cmdlet(VerbsDiagnostic.Test, "ToExchangeCmdlet")]
+    [OutputType(typeof(SendQueueResult))]
+    public class ToExchangeCmdletCommand : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string TargetName { get; set; }
+
+        [Parameter(
+            Position = 1,
+            ValueFromPipelineByPropertyName = true)]
+        //[ValidateSet("Cat", "Dog", "Horse")]
+        public string Message { get; set; } = "Hello World";
+
+        [Parameter(
+            Position = 2,
+            ValueFromPipelineByPropertyName = true)]
+        public string MQHost { get; set; } = "localhost";
+
+        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("Begin!");
+        }
+
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessRecord()
+        {
+            var result = new SendQueueResult
+            {
+                QueueName = TargetName,
+                Message = Message,
+                MQHost = MQHost
+            };
+
+            WriteObject(result.SendMessage());
+        }
+
+        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing()
+        {
+            WriteVerbose("End!");
+        }
+    }
+
+
+}
